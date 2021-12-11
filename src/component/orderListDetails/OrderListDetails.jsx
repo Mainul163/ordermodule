42db5da7d759f8739adf455819e339d4d4520766
Mainul islam <mainulisraf163@gmail.com>
2021-12-11 20:32:11 +0600

    still working

--- src/component/orderListDetails/OrderListDetails.jsx
+++ src/component/orderListDetails/OrderListDetails.jsx
@@ -1,21 +1,23 @@
-import React from "react";
-import { Card } from "antd";
-import "./OrderListDetails.css";
+import React from 'react';
+import { Card } from 'antd';
+import './OrderListDetails.css';
 const OrderListDetails = ({ branchOrderFoodAdditionals }) => {
-  return (
-    <div className="site-card-border-less-wrapper">
-      {branchOrderFoodAdditionals.map((data, index) => (
-        <Card
-          title={data.title}
-          key={index}
-          bordered={false}
-          style={{ width: 300, marginBottom: "10px" }}
-        >
-          <p>Card content</p>
-        </Card>
-      ))}
-    </div>
-  );
+	return (
+		<div className="site-card-border-less-wrapper">
+			{branchOrderFoodAdditionals.map((data, index) => (
+				<Card
+					title={data.title}
+					key={index}
+					bordered={false}
+					style={{ width: 300, marginBottom: '10px' }}
+				>
+					<p>Quantity: {data.quantity}</p>
+					<br />
+					<p>Regular Price: {data.regularPrice}</p>
+				</Card>
+			))}
+		</div>
+	);
 };
 
 export default OrderListDetails;
